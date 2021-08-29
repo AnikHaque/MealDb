49683fd769466db0b5ccd288caf9795a216a6dd0
EkramulHaqueEngineer <ekramulhaqueengineer@gmail.com>
2021-08-29 15:49:50 +0600

    added

--- mealdb.js
+++ mealdb.js
@@ -1,1 +1,6 @@
-console.log('connected');
+const displayMeal = () => {
+    const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=$(meals)"
+fetch(url)
+.then(res => res.json())
+.then(data => console.log(data.meals))
+}
